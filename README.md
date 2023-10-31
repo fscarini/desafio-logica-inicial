f493fd3daba5bcdc64d06440e3b54b2201da43b4
Fernando Carini <fernando2015lk@gmail.com>
2023-10-31 14:57:02 -0300

    Correção dos níveis de XP, e adição do README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Classificador de Nível de Herói
+
+Olá! Seja bem-vindo a minha resolução de um desafio proposto pela plataforma da DIO!
+O desafio feito foi retirado do bootcamp Potência Tech iFood - Programação do Zero, o desafio tem o objetivo de aplicar conhecimentos básicos da linguagem de JavaScript em um "Classificador de Nível de Herói".
+
+# Quais estruturas foram utilizadas? ❓
+
+Foram utilizadas variáveis para armazenar o Nome do herói, e a quantidade de XP que ele possui.
+
+Por outro lado, usei a estrutura condicional de IF com operadores de comparação para verificar a quantidade de XP que o Herói possui, tudo isso dentro de uma estruta Switch, que teve como parâmetro o contador "i" que fez parte da estrutura de loop FOR.
--- index.js
+++ index.js
@@ -1,53 +1,59 @@
 let nomeDoHero = "Nandinho do Skate 3"
-let xpHero = 7000
+let xpHero = 9000
 let i 
 
-for(i = 1; i <= 8; i++){
+for(i = 1; i <= 9; i++){
     switch (i) {
         case 1:
             if (xpHero <= 1000) {
+                console.log(`${nomeDoHero} é classificado com o nível Ferro`)
+            }
+            break;
+
+        case 2:
+            if (xpHero >= 1001 && xpHero <= 2000) {
                 console.log(`${nomeDoHero} é classificado com o nível Bronze`)
             }
             break;
     
-        case 2:
-            if (xpHero >= 1001 && xpHero <= 2000) {
+        case 3:
+            if (xpHero >= 2001 && xpHero <= 5000) {
                 console.log(`${nomeDoHero} é classificado com o nível Prata`)
             }
             break;
     
-        case 3:
-            if (xpHero >= 2001 && xpHero <= 3000) {
+        case 4:
+            if (xpHero >= 5001 && xpHero <= 7000) {
                 console.log(`${nomeDoHero} é classificado com o nível Ouro`)
             }
             break;
     
-        case 4:
-            if (xpHero >= 3001 && xpHero <= 4000) {
+        case 5:
+            if (xpHero >= 7001 && xpHero <= 8000) {
                 console.log(`${nomeDoHero} é classificado com o nível Platina`)
             }
             break;
     
-        case 5:
-            if (xpHero >= 4001 && xpHero <= 5000) {
+        case 6:
+            if (xpHero >= 8001 && xpHero <= 9000) {
                 console.log(`${nomeDoHero} é classificado com o nível Diamante`)
             }
             break;
     
-        case 6:
-            if (xpHero >= 5001 && xpHero <= 6000) {
+        case 7:
+            if (xpHero >= 9001 && xpHero <= 10000) {
                 console.log(`${nomeDoHero} é classificado com o nível Ascendente`)
             }
             break;
     
-        case 7:
-            if (xpHero >= 6001 && xpHero <= 7000) {
+        case 8:
+            if (xpHero >= 10001 && xpHero <= 12000) {
                 console.log(`${nomeDoHero} é classificado com o nível Imortal`)
             }
             break;
     
-        case 8:
-            if (xpHero >= 7001) {
+        case 9:
+            if (xpHero >= 12001) {
                 console.log(`${nomeDoHero} é classificado com o nível Radiante`)
             }
     }
